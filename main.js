06aab0a4c5168d6ba1d12b76629a33d6ebd547eb
Sycoms <77230943+Benyamin35@users.noreply.github.com>
2022-12-21 17:55:51 +0400

    aaa

--- main.js
+++ main.js
@@ -6,48 +6,17 @@ function checkTheSentence (input) {
     input.addEventListener('keypress', (e) => {
         if(e.key === 'Enter') {
             let sentence = input.value;
-            const arrayOfSentence = sentence.split(' ');
-            for(let i = 0; i < arrayOfSentence.length; i++ ){
-                checkTheLetterForSentence(arrayOfSentence[i], sentence)
-            }
-
+            const classicText = sentence.split(" ").map(checkTheLetterForSentence).join(" ")
+            console.log(classicText);
         }
 
     })
 }
-function checkTheLetterForSentence (word, sentence) {
-//    console.log(sentence)
-  //  console.log(word)
-    
+function checkTheLetterForSentence (word) {
     const letter = word.charAt(word.length-1);
-    addLetter(letter, word, sentence)
+    const needToChange = ["ա", "ո"].includes(letter);
+    return needToChange ? word + "յ" : word;
+    
 }
-
-function replaceTheSentenceWords () {
-
-}
-
 
 checkTheSentence(input)
-
-
-// function checkTheLetter(input){
-//     input.addEventListener('keypress', (e) => {
-//         if (e.key === 'Enter') {
-//             const inputWord = input.value;
-//             console.log(inputWord)
-//             const letter = inputWord.charAt(inputWord.length-1);
-//             addLetter(letter, inputWord);
-//         }
-//     });
-// }
-
-function addLetter (letter, inputWord, sentence) {
-    if(letter === "ա" || letter === "ո"){
-        const newWord = inputWord + "յ";
-        let a = sentence.replace(inputWord, newWord)
-        console.log(a)
-        result.innerHTML = a;
-    } 
-}
-// checkTheLetter(input)
